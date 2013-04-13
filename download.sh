68207153127f0d5aaaff48693eeadcc7a194b46a
Thomas Levine <occurrence@thomaslevine.com>
2013-04-13 15:49:48 -0400

    resume

--- download.sh
+++ download.sh
@@ -13,17 +13,24 @@ rnorm() {
   ))
 }
 
-dir="downloads/$(date --rfc-3339 seconds)"
-mkdir -p "${dir}"
+dir="downloads/$(date --rfc-3339 date)"
+nTries=10
+
+if test -d "${dir}"; then
+  startingPage=$(($nTries + $(ls "${dir}"|sort -n|tail -n1|cut -d. -f1)))
+else
+  mkdir -p "${dir}"
+  startingPage=1
+fi
 
 failuresInARow=0
-for projectId in $(seq 1 200); do
+for projectId in $(seq "${startingPage}" 200); do
   file="${dir}/${projectId}.html"
 
   wget -O "${file}" "http://collabfinder.com/project/${projectId}" || failuresInARow=$(($failuresInARow + 1))
   sleep $(rnorm)s
 
-  if test 10 -lt $failuresInARow; then
+  if test $nTries -lt $failuresInARow; then
     echo Stopped after non-project number $projectId
     exit 0
   fi
