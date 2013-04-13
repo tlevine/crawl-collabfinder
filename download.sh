4eda6c04da12efa69dbe311efc16f1ebd2db2417
Thomas Levine <occurrence@thomaslevine.com>
2013-04-13 15:11:13 -0400

    404 errors

--- download.sh
+++ download.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 set -e
 
+# This converges to normality by the central limit theorem
+rnorm() {
+  echo $((
+    $(($RANDOM % 8)) +
+    $(($RANDOM % 8)) +
+    $(($RANDOM % 8)) +
+    $(($RANDOM % 8)) +
+    $(($RANDOM % 8)) +
+    $(($RANDOM % 8))
+  ))
+}
+
 dir="downloads/$(date --rfc-3339 seconds)"
 mkdir -p "${dir}"
 
@@ -8,11 +20,8 @@ failuresInARow=0
 for projectId in $(seq 1 200); do
   file="${dir}/${projectId}.html"
 
-  wget -O "${file}" "http://collabfinder.com/project/${projectId}"
-
-  if grep "We couldn't find the page you requested." "${file}"; then
-    failuresInARow=$(($failuresInARow + 1))
-  fi
+  wget -O "${file}" "http://collabfinder.com/project/${projectId}" || failuresInARow=$(($failuresInARow + 1))
+  sleep $(rnorm)s
 
   if test 10 -lt $failuresInARow; then
     echo Stopped after non-project number $projectId
