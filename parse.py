9ed75dbad888d6a02b440d819f2645915579690f
Thomas Levine <occurrence@thomaslevine.com>
2013-04-13 15:44:19 -0400

    extract steps 1 and 2

--- parse.py
+++ parse.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python2
 from lxml.html import parse
 
-def backgrounds(html):
+def _backgrounds(html):
     'This project needs...'
-    b = {
-        'Developers': False,
-        'Designers': False,
-        'Artists': False,
-        'Writers': False,
-        'Scientists': False,
-    }
-    text_nodes = set(map(unicode, html.xpath('id("seeking")/descendant::b/text()')))
-    for k in b.keys():
-        if k in text_nodes:
-            b[k] = True
-    return b
+    return set(map(unicode, html.xpath('id("seeking")/descendant::b/text()')))
 
+def _description(html):
+    '''
+    What are you making?
+    Why are you making it?
+    What do you need help with?
+    '''
+    questions =['what', 'why', 'need']
+    answers = filter(None, map(unicode, html.xpath('//div[@class="column span-18 append-1 border_t_g first last"]/h2[@class="plain"]/text()')))
+    return zip(questions, answers)
+
+def _goals(html):
+    'Project Goals'
+    return map(unicode, html.xpath('id("project_goals")/li/text()'))
+
+def _tags(html):
+    'Tags'
+    return map(unicode, html.xpath('//ul[@class="bottomend-1 attributes tags"]/li/a/text()'))
+
+def _github(html):
+    'Project Github'
+    results = html.xpath('//h2[@class="toppend-1 border_t_g"]/a/text()')
+    if len(results) == 1:
+        return unicode(url)
 
 def answers(html):
-    return backgrounds(html)
+    return {
+        'backgrounds': _backgrounds(html),
+        'description': _description(html),
+        'goals': _goals(html),
+        'tags': _tags(html),
+        'github': _github(html),
+    }
 
 
 if __name__ == '__main__':
