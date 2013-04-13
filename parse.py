b3bc43d445fe73c0f2dc98f6fa18884043d51a55
Thomas Levine <occurrence@thomaslevine.com>
2013-04-13 15:27:43 -0400

    backgrounds

--- parse.py
+++ parse.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python2
+from lxml.html import parse
+
+def backgrounds(html):
+    'This project needs...'
+    b = {
+        'Developers': False,
+        'Designers': False,
+        'Artists': False,
+        'Writers': False,
+        'Scientists': False,
+    }
+    text_nodes = set(map(unicode, html.xpath('id("seeking")/descendant::b/text()')))
+    for k in b.keys():
+        if k in text_nodes:
+            b[k] = True
+    return b
+
 
 def answers(html):
-
+    return backgrounds(html)
 
 
 if __name__ == '__main__':
