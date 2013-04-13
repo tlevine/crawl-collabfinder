5509f8a17e5977da7308298e79da3e0c9c5f03e3
Thomas Levine <occurrence@thomaslevine.com>
2013-04-13 17:36:39 -0400

    almost

--- project-generate.py
+++ project-generate.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python2
+from nltk.tokenize import wordpunct_tokenize #, word_tokenize, sent_tokenize
+from nltk.probability import LidstoneProbDist #LaplaceProbDist
+from nltk import NgramModel
 
 
+def train(descriptions):
+    '''
+    :param descriptions: a list of training texts
+    :type descriptions: list(list(str))
+    '''
+    train = wordpunct_tokenize(descriptions)
+    lm = NgramModel(3, train, estimator = (lambda fdist, bins: LidstoneProbDist(fdist, 0.2)))
+    return lm
+
+def main(descriptions):
+    for subdescription in ['what', 'why', 'need']:
+        lm = train((d[subescription] for d in descriptions))
 
 if __name__ == '__main__':
     import os
     import json
 
-    descriptions = filter(None, [project['description'] for project in json.load(open(os.path.join('output', 'projects.json')))])
+    descriptions = filter(None, (project['description'] for project in json.load(open(os.path.join('output', 'projects.json')))))
+    main(descriptions)
