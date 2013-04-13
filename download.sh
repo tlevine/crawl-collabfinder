4d005f8882e73a6d374598d8f0558934af0edf4d
Thomas Levine <occurrence@thomaslevine.com>
2013-04-13 14:57:40 -0400

    handle errors

--- download.sh
+++ download.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
+set -e
 
 dir="downloads/$(date --rfc-3339 seconds)"
 mkdir -p "${dir}"
 
 failuresInARow=0
 for projectId in $(seq 1 200); do
-  wget -O "${dir}/${projectId}.html" "http://collabfinder.com/project/${projectId}"
+  file="${dir}/${projectId}.html"
+
+  wget -O "${file}" "http://collabfinder.com/project/${projectId}"
+
+  if grep "We couldn't find the page you requested." "${file}"; then
+    failuresInARow=$(($failuresInARow + 1))
+  fi
+
+  if test 10 -lt $failuresInARow; then
+    echo Stopped after non-project number $projectId
+    exit 0
+  fi
 done
